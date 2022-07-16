f263fd7ab2822a5cff454523eea07150579bf7d4
sammy-jay <soneyetolulope4@gmail.com>
2022-07-16 16:43:12 +0100

    implemented some views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Basic way of sending a response, also takes number as a param
+def hello(request, number):
+    text = "<h1>Hello, welcome to my page</h1>" + str(number)
+    return HttpResponse(text)
+
+
+# Using the render function
+def template(request):
+    return render(request, "myapp/template/hello.html", {})
