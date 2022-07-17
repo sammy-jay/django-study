8fd28c7e739795971e5ac3a98fef890cfaa23c16
sammy-jay <soneyetolulope4@gmail.com>
2022-07-17 21:38:24 +0100

    using get_object_or_404 for error handling

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 from .models import Question
@@ -11,12 +11,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    
-    return return render(request, 'polls/detail.html', {"question": question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {"question": question})
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}"
