65e024ed062f605f59492145f93d6a0beb63a213
sammy-jay <sammyjayden27@gmail.com>
2022-07-19 22:00:00 +0100

    quick example on models

--- deepdive/models.py
+++ deepdive/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+"""
+A model is the single, definitive source of information about your data
+Each model maps to a database table
+"""
 
+# Quick Example
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+# Simply add the app to settings.py
+# run python manage.py makemigrations
+# then run python manage.py migrate
